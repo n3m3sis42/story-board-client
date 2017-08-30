1611ddc67f9f1458383b75e7b7c827b62bc5044d
n3m3sis42 <n3m3sis42@gmail.com>
2017-08-30 18:27:24 -0400

    completed bare bones page with scene cards that can be added, updated and deleted -- changes persist to db

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Scene = ({scene}) => {
-  return (
-    <div className="tile">
-      <div className="tile-header">
-        {scene.title}
+export default class Scene extends Component {
+
+  handleClick = () => {
+    this.props.onClick(this.props.scene.id)
+  }
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.scene.id)
+  }
+
+  render() {
+    return (
+      <div className="tile">
+        <span className="delete" onClick={this.handleDelete}>x</span>
+        <h4 onClick={this.handleClick}>{this.props.scene.title}</h4>
+        <p onClick={this.handleClick}>{this.props.scene.notes}</p>
       </div>
-      {scene.notes}
-    </div>
-  )
+    )
+  }
 }
-
-export default Scene
--- src/components/ScenesContainer.js
+++ src/components/ScenesContainer.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import ScenesAdapter from '../adapters/ScenesAdapter'
 import Scene from './Scene'
+import SceneForm from './SceneForm'
+
+const data = {
+  scene: {
+    title: '',
+    notes: ''
+  }
+}
 
 export default class ScenesContainer extends Component {
 
@@ -8,7 +16,9 @@ export default class ScenesContainer extends Component {
     super(props)
 
     this.state = {
-      scenes: []
+      scenes: [],
+      currentSceneId: null,
+      notification: ''
     }
   }
 
@@ -17,11 +27,72 @@ export default class ScenesContainer extends Component {
       .then(json => this.setState({
               scenes: json
             },
-            () => {console.log(this.state.scenes)}
-          )
-        )
-      .catch(error => console.log(error))
+            () => {console.log(this.state.scenes)})
+      )
+  }
 
+  addNewScene = () => {
+    ScenesAdapter.create(data)
+      .then(json => this.setState({
+                      scenes: [json, ...this.state.scenes],
+                      currentSceneId: json.id,
+                      notification: ''
+                    })
+      )
+  }
+
+  getSceneIndex = (id) => {
+    return this.state.scenes.findIndex(scene => scene.id === id)
+  }
+
+  updateScene = (data) => {
+    ScenesAdapter.update(this.state.currentSceneId, data)
+      .then(json => {
+                      let index = this.getSceneIndex(json.id)
+                      this.setState({
+                        scenes: [...this.state.scenes.slice(0, index), json, ...this.state.scenes.slice(index + 1)],
+                        currentSceneId: json.id,
+                        notification: 'All changes saved.'
+                      },
+                        () => {console.log(this.state.scenes)}
+                      )
+      })
+  }
+
+  resetNotification = () => {
+    this.setState({
+      notification: ''
+    })
+  }
+
+  enableEditing = (id) => {
+    this.setState({
+      currentSceneId: id
+    }, () => { this.title.focus() })
+  }
+
+  deleteScene = (id) => {
+    ScenesAdapter.delete(id)
+      .then(resp => {
+                      let index = this.getSceneIndex(id)
+                      if (resp.status === 204) {
+                          this.setState({
+                            scenes: [...this.state.scenes.slice(0, index), ...this.state.scenes.slice(index + 1)],
+                            currentSceneId: '',
+                            notification: 'Scene deleted successfully.'
+                          },
+                            () => {console.log(this.state.scenes)}
+                          )
+                      } else {
+                        this.setState({
+                          currentSceneId: '',
+                          notification: 'Unable to delete scene.'
+                        },
+                          () => {console.log(resp.status, resp.statusText)}
+                        )
+
+                      }
+      })
   }
 
   render() {
@@ -29,17 +100,24 @@ export default class ScenesContainer extends Component {
       <div className="Scenes-container">
         <div className="Scenes-container header">
           <h3>Scenes</h3>
-          <div>
-            <button className="Scenes-container btn">
-              New Scene
-            </button>
-          </div>
         </div>
+        <div className="Scenes-container controls">
+          <button className="Scenes-container btn" onClick={this.addNewScene} >
+            +
+          </button>
+          <span className="Scenes-container notification">
+            {this.state.notification}
+          </span>
+        </div>
+        <div className="Scenes-container body">
           {this.state.scenes.map((scene) => {
             return (
-              <Scene key={scene.id} scene={scene} />
+              (this.state.currentSceneId === scene.id) ?
+              <SceneForm key={scene.id} scene={scene} resetNotification={this.resetNotification} titleRef={input => this.title = input} updateScene={this.updateScene} /> :
+              <Scene key={scene.id} scene={scene} onClick={this.enableEditing} onDelete={this.deleteScene}/>
             )
           })}
+        </div>
       </div>
     );
   }
